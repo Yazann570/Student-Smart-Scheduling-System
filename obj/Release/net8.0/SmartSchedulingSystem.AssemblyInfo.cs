a0338e6992ad6f9c8f1e5835e894b2dc360aa8c1
unknown <laila.sadideen2004@gmail.com>
2026-03-06 17:00:18 +0300

    changed the background color

--- obj/Release/net8.0/SmartSchedulingSystem.AssemblyInfo.cs
+++ obj/Release/net8.0/SmartSchedulingSystem.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartSchedulingSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f47c6a31f43c76b93ad5b14179ac33b44450c29")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartSchedulingSystem")]
